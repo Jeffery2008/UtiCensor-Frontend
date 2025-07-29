ec23c727a7d595d3bbf7c2fb0cf19fbc09a1f418
Jeffery2008 <3510862791@qq.com>
2025-07-30 07:55:51 +0800

    refactor: update API base URL to use environment variable

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://154.37.213.151:8081/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // 创建axios实例
 const api = axios.create({
